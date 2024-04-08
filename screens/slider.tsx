0c8b132e4a01e998e38f2f81342715f1c4757b33
munezeromicha <munezerontaganiramichel@gmail.com>
2024-04-08 12:51:56 +0200

    Routing and design added on the first page

--- screens/slider.tsx
+++ screens/slider.tsx
@@ -1,23 +1,25 @@
-import { StyleSheet, Text, View, Image, Dimensions,TouchableOpacity } from 'react-native';
-import Asset1 from '../assets/Asset1.png'
-import Asset2 from '../assets/Asset2.png'
-import Asset3 from '../assets/Asset3.png'
-import Message from '../assets/Message.png'
-import Call from '../assets/Call.png'
-import { useState } from 'react';
-import AppIntroSlider from 'react-native-app-intro-slider';
 import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import Asset1 from '../assets/Asset1.png';
+import Asset2 from '../assets/Asset2.png';
+import Asset3 from '../assets/Asset3.png';
+import Message from '../assets/Message.png';
+import Call from '../assets/Call.png';
+import AppIntroSlider from 'react-native-app-intro-slider';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
+import loginScreen from './LoginScreen';
 
 type SliderScreenProps = {
   navigation: StackNavigationProp<any, 'SliderScreen'>;
 };
+
 const slides = [
   {
     id: 1,
     title: "Welcome",
-    description: "Find a best possible way to park",
+    description: "Find the best possible way to park",
     image: Asset1
   },
   {
@@ -32,13 +34,13 @@ const slides = [
     description: "Find your perfect parking space wherever and whenever you need",
     image: Asset3
   }
-]
+];
 
 const Slider: React.FC<SliderScreenProps> = ({ navigation }) => {
-  const [showHomePage, setShowHomePage] = useState(false)
+  const [showHomePage, setShowHomePage] = useState(false);
 
   const buttonLabel = (label) => {
-    return(
+    return (
       <View style={{
         position: 'absolute',
         top: 40,
@@ -51,91 +53,97 @@ const Slider: React.FC<SliderScreenProps> = ({ navigation }) => {
           {label}
         </Text>
       </View>
-    )
+    );
+  };
+
+  if (!showHomePage) {
+    return (
+      <View style={styles.container}>
+        <AppIntroSlider
+          data={slides}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.swipePart}>
+                <Image
+                  source={item.image}
+                  style={styles.image}
+                  resizeMode='contain'
+                />
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+              </View>
+            );
+          }}
+          activeDotStyle={{
+            marginTop: 30,
+            backgroundColor: '#F43939',
+          }}
+          dotStyle={{
+            marginTop: 30,
+            backgroundColor: '#c4c4cb',
+          }}
+          showSkipButton
+          showNextButton={false}
+          showDoneButton={false}
+          renderSkipButton={() => buttonLabel("Skip")}
+          onSkip={() => {
+            setShowHomePage(true);
+          }}
+        />
+       
+        <View style={styles.nonSwipingContent}>
+          <TouchableOpacity style={styles.buttonOne} onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.buttonText}><Image source={Message} />Login with Email</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonTwo}>
+            <Text style={styles.buttonTextTwo}><Image source={Call} />Login with Phone</Text>
+          </TouchableOpacity>
+          <View style={styles.bottomText}>
+            <Text style={styles.lastText}>Don't have an account? <Text style={styles.signupText}>Signup</Text></Text>
+          </View>
+        </View>
+      </View>
+    );
   }
-
-  if(!showHomePage){
-    return(
-      <AppIntroSlider 
-        data={slides}
-        renderItem={({item})=>{
-          return(
-            <View style={styles.container}>
-              <Image 
-                source={item.image}
-                style={styles.image}
-                resizeMode='contain' 
-              />
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.description}>{item.description}</Text>
-            </View>
-            
-          )
-        }}
-        activeDotStyle={{
-          backgroundColor: '#F43939',
-        }}
-        showSkipButton
-        showNextButton={false}
-        showDoneButton={false}
-        renderSkipButton={()=>buttonLabel("Skip")}
-        onSkip={()=>{
-          
-          setShowHomePage(true);
-        }}
-      />
-
-
-    )
-  }
-
-}
-
-export default Slider;
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E0E0E0',
-    alignItems: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // gap: 10,
+  },
+  swipePart: {
     justifyContent: 'center',
-    gap: 10,
+    alignItems: 'center',
   },
-  slider: {
-
-zIndex: 9999,
-  },
-  title:{
+  title: {
+    marginTop: 30,
     fontSize: 24,
-    color: '#2D2D2D'
+    color: '#2D2D2D',
+    textAlign: 'center',
   },
-  description : {
-    
+  description: {
+    marginTop: 10,
     fontSize: 16,
     color: '#2D2D2D',
     width: 250,
     alignContent: 'center',
     justifyContent: 'center',
     textAlign: 'center',
-
-  },
-  textStyle: {
-    color: 'white',
   },
   image: {
-    marginTop: 180,
+    marginTop: 140,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  buttons: {
-    flex: 4,
-    justifyContent: 'center',
+  nonSwipingContent: {
     alignItems: 'center',
-    gap: 15,
-    marginTop: 450,
-    // borderWidth: 1,
-},
-buttonOne: {
+    marginTop: 20,
+  },
+  buttonOne: {
     width: 300,
     height: 50,
     backgroundColor: '#2D2D2D',
@@ -143,33 +151,38 @@ buttonOne: {
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    
-},
-buttonText: {
+    marginVertical: 10,
+  },
+  buttonText: {
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
-
-},
-buttonTwo: {
+  },
+  buttonTwo: {
     width: 300,
     height: 50,
     backgroundColor: '#FFFFFF',
     borderRadius: 15,
     justifyContent: 'center',
     alignItems: 'center',
-},
-buttonTextTwo: {
+    marginVertical: 10,
+  },
+  buttonTextTwo: {
     color: '#2D2D2D',
     fontSize: 18,
     fontWeight: 'bold',
-},
-bottomText:{
-    flex: 5,
+  },
+  bottomText: {
     justifyContent: 'space-around',
-},
-lastText: {
+    marginVertical: 20,
+  },
+  lastText: {
     fontSize: 14,
     color: '#2D2D2D',
-},
+  },
+  signupText :{
+    color: '#F43939'
+  }
 });
+
+export default Slider;
